b17f028f1f27b2525d2925037d55060f1c1fd2f8
gmpal <g.m.pal95@gmail.com>
2022-12-08 15:54:52 +0100

    generator

--- generator/generator.py
+++ generator/generator.py
@@ -9,6 +9,7 @@ class Generator():
     
     def __init__(self, n_customers=50, n_terminals=10, radius=20, nb_days=8, start_date="2018-04-01", random_state = 2, \
         max_days_from_compromission=3):
+
         self.n_customers = n_customers
         self.n_terminals = n_terminals
         self.radius = radius
@@ -28,17 +29,43 @@ class Generator():
     
 
         
-    def generate(self):
+    def generate(self) -> None:
+        """Generates transactions data for the customers in this dataset.
+
+        This function groups the customer profiles by customer ID and then generates
+        transactions data for each group using the `_generate_transactions_table`
+        function. The generated transactions data is stored in the `transactions_df`
+        attribute of this object.
+
+        Returns:
+            None
+        """
         
         groups = self.customer_profiles_table.groupby('CUSTOMER_ID')
         self.transactions_df = groups.apply(lambda x: self._generate_transactions_table(customer_profile=x.iloc[0])).reset_index(drop=True)    
 
-    def export(self, filename='transactions.csv', format='csv'):
+    def export(self, filename: str ='transactions.csv', format: str ='csv') -> None:
+        """Exports the transactions data to a file.
+
+        This function saves the transactions data stored in the `transactions_df`
+        attribute of this object to a file with the specified `filename` and
+        `format`. If `format` is 'csv', the data is saved as a CSV file. If
+        `format` is 'pkl', the data is pickled and saved to a binary file.
+
+        Args:
+            filename (str): The name of the file to save the data to.
+            format (str): The format to use for saving the data. Must be 'csv' or
+                'pkl'.
+
+        Returns:
+            None
+        """
         if format=='csv':
             self.transactions_df.to_csv(filename,index=False)
         elif format=='pkl':
             with open(filename, 'wb') as f:
                 pickle.dump(self.transactions_df,f)
+
 
     def _generate_transactions_table(self, customer_profile):
         days_from_compromission = 0  # Number of max frauds days
