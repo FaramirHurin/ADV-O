e27e63957b8cd11c87fd7fcae4ffb3cecfd98f4b
gmpal <g.m.pal95@gmail.com>
2022-12-13 21:59:49 +0100

    Updated main for ADVO'

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from generator.generator import Generator
+import pandas as pd
 
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import GridSearchCV
 
-generator = Generator(n_customers=100, n_terminals=50)
-generator.generate()
-generator.export()
+from experiments.advo import ADVO
+
+def main():
+    # Create an ADVO instance
+    advo = ADVO()
+
+    # Generate transactions data using the ADVO instance
+    advo.generate_transactions(n_customers=100, n_terminals=10)
+
+    # Create pairs of fraudulent transactions using the ADVO instance
+    advo.create_couples()
+
+    # Tune the hyperparameters of a Ridge regression model using a GridSearchCV instance
+    searcher = GridSearchCV(Ridge(), {'alpha': [0.1, 1, 10]}, cv=3)
+    advo.tune_regressors(searcher, Ridge)
+
+    # Fit the tuned regression models to the data using the ADVO instance
+    advo.fit_regressors(pd.DataFrame.mean_squared_error)
+
+    # Generate synthetic transactions using the ADVO instance
+    synthetic_transactions = advo.generate_synthetic_transactions(n_samples=100)
+
+    # Print the generated synthetic transactions
+    print(synthetic_transactions)
+
+if __name__ == '__main__':
+    main()
