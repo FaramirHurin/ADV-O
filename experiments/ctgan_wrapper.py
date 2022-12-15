48afec8bffaf1f2a0e881b8ad75a48c0475c478f
gmpal <g.m.pal95@gmail.com>
2022-12-15 20:02:29 +0100

    fixed ctgan wrapper

--- experiments/ctgan_wrapper.py
+++ experiments/ctgan_wrapper.py
@@ -12,7 +12,9 @@ class CTGANOverSampler():
         frauds_df = df.loc[df['TX_FRAUD']==1]
         num_frauds = frauds_df.shape[0]
         num_synthetic_frauds = int((self.sampling_strategy)*df.shape[0]) - num_frauds
-        synthetic_data = CTGAN(epochs=self.epochs, cuda=self.cuda).fit(frauds_df).sample(num_synthetic_frauds)
+        ctgan = CTGAN(epochs=self.epochs, cuda=self.cuda)
+        ctgan.fit(frauds_df)
+        synthetic_data = ctgan.sample(num_synthetic_frauds)
         augmented_df = pd.concat([synthetic_data, frauds_df], axis=0)
         return augmented_df.drop(columns=['TX_FRAUD']), augmented_df['TX_FRAUD']
 
