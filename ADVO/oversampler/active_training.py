f02a4ff1c0875a9d46989b816dab7d77b5873cf9
FaramirHurin <dalunghi@gmail.com>
2023-03-04 13:10:44 +0100

    Now the Gaussian makes some sense

--- ADVO/oversampler/active_training.py
+++ ADVO/oversampler/active_training.py
@@ -8,53 +8,14 @@ from numpy import *
 import math
 from scipy.stats import multivariate_normal
 
-
-
-
-
-predictions = np.array([[2, 3, 4, 5, 6],  [2, 3, 4, 5, 6]]).transpose()
-values =  np.array([[2.2, 3, 5, 5, 6], [2, 3, 4, 5, 6]]).transpose()
+predictions = np.array([[2, 3],  [3.3, 2.1], [0, 0.2]])
+values =  np.array([[1, 3], [2, 3], [0.2, 1]])
 errors = values - predictions
-x = errors
 
-# mean vector
-mu = np.array([2,3,8,10,3]).reshape(-1,1)
-mu2 = [2,3,8,10,3]
+mean = np.mean(errors, axis=0)
+cov = np.cov(errors, rowvar=0)
 
-# covariance matrix
-sigma = matrix([[1, 0, 0, 0, 0.2],
-           [0, 1, 0, 0, 0.3],
-            [0.5, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0],
-            [0.3, 0, 0, 0, 1]
-          ])
 
-generator = multivariate_normal(mean= mu2, cov=sigma, allow_singular=True)
-generator.rvs(10)
+generator = multivariate_normal(mean= mean, cov=cov, allow_singular=True)
+print(generator.rvs(10))
 
-def pdf_multivariate_gauss(x, mu, cov):
-    '''
-    Caculate the multivariate normal density (pdf)
-
-    Keyword arguments:
-        x = numpy array of a "d x 1" sample vector
-        mu = numpy array of a "d x 1" mean vector
-        cov = "numpy array of a d x d" covariance matrix
-    '''
-    assert(mu.shape[0] > mu.shape[1]), 'mu must be a row vector'
-    assert(x.shape[0] > x.shape[1]), 'x must be a row vector'
-    assert(cov.shape[0] == cov.shape[1]), 'covariance matrix must be square'
-    assert(mu.shape[0] == cov.shape[0]), 'cov_mat and mu_vec must have the same dimensions'
-    assert(mu.shape[0] == x.shape[0]), 'mu and x must have the same dimensions'
-    part1 = 1 / ( ((2* np.pi)**(len(mu)/2)) * (np.linalg.det(cov)**(1/2)) )
-    part2 = (-1/2) * ((x-mu).T.dot(np.linalg.inv(cov))).dot((x-mu))
-    return float(part1 * np.exp(part2))
-
-def test_gauss_pdf(errors, mu, sigma):
-    x = errors
-    mu  = mu
-    cov = sigma
-
-    print(pdf_multivariate_gauss(x, mu, cov))
-
-test_gauss_pdf(errors=errors, mu=mu, sigma=sigma)
