12c3686ac8374a1da5fe7dc825210745dca7fe1b
FaramirHurin <dalunghi@gmail.com>
2023-03-04 13:12:45 +0100

    Now the Gaussian makes some sense

--- ADVO/oversampler/active_training.py
+++ ADVO/oversampler/active_training.py
@@ -1,21 +1,22 @@
-#Idea. Fit a multivariate Gaussian on the errors, make predictins for ADV-O as most likely fraud + draw from Gaussian.
-#Then filter through classifier. Since no library allows to fit a multivariate normal dist, I am experimenting to do so.
-
-
+#Idea. Fit a multivariate Gaussian on the errors,
+# make predictins for ADV-O as most likely fraud + draw from Gaussian.
+#Finally, filter through classifier.
 
 import numpy as np
 from numpy import *
-import math
 from scipy.stats import multivariate_normal
 
-predictions = np.array([[2, 3],  [3.3, 2.1], [0, 0.2]])
-values =  np.array([[1, 3], [2, 3], [0.2, 1]])
-errors = values - predictions
+'''Placeholder for the module importing the predictions and true frauds from the ADV-O training set'''
+def errors_placeholder():
+    predictions = np.array([[2, 3], [3.3, 2.1], [0, 0.2]])
+    values = np.array([[1, 3], [2, 3], [0.2, 1]])
+    return values - predictions
+
+errors = errors_placeholder()
+
 
 mean = np.mean(errors, axis=0)
 cov = np.cov(errors, rowvar=0)
-
-
 generator = multivariate_normal(mean= mean, cov=cov, allow_singular=True)
 print(generator.rvs(10))
 
