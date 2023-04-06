3e5f22b47f9965ef4dba65d65b15e15753f9f62d
Gian Marco Paldino <g.m.pal95@gmail.com>
2023-04-06 15:48:21 +0200

    timegan

---
+++ ADVO/oversampler/timegan_wrapper.py
@@ -0,0 +1,23 @@
+#from ctgan import CTGAN
+import pandas as pd 
+
+class TimeGANOverSampler():
+    def __init__(self, sampling_strategy, epochs=10, cuda=True):
+        self.sampling_strategy = sampling_strategy
+        self.epochs = epochs
+        self.cuda = cuda
+
+    def fit_resample(self, train_X, train_Y):
+        df = pd.concat([train_X, train_Y], axis=1)
+        frauds_df = df.loc[df['TX_FRAUD']==1]
+        num_frauds = frauds_df.shape[0]
+        num_synthetic_frauds = int((self.sampling_strategy)*df.shape[0]) - num_frauds
+        #ctgan = CTGAN(epochs=self.epochs, cuda=self.cuda)
+        ctgan = None
+        ctgan.fit(frauds_df)
+        synthetic_data = ctgan.sample(num_synthetic_frauds)
+
+        augmented_df = pd.concat([synthetic_data, df], axis=0)
+
+        return augmented_df.drop(columns=['TX_FRAUD']), augmented_df['TX_FRAUD']
+
